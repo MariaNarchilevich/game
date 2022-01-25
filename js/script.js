a40a1539aa743e830fc8afc63f355b1ab0802a3e
MariaNarchilevich <87192847+MariaNarchilevich@users.noreply.github.com>
2022-01-25 13:06:55 +0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -22,11 +22,23 @@ let num2_res = document.querySelector(".res"); //2 задание
 let num3_task = document.querySelector(".flex-column3"); //3задание
 let num3_res = document.querySelector(".res3"); //3 задание
 
+let user_cscore = 0;
+let user_cscore1 = 0;
+let user_cscore2 = 0;
+let user_cscore3 = 0;
+
+let section_ball = document.querySelector(".user-score");
+
+const ball2 = document.createElement("div");
+ball2.classList.add("score2");
+
+const ball = document.createElement("div");
+ball.classList.add("score");
+
 // старт
 button_start.addEventListener("click", function () {
   button_start.classList.add("d-none");
   form.classList.remove("d-none");
-  //login();
 });
 
 // заполнение формы
@@ -37,6 +49,8 @@ binput_form.addEventListener("click", function () {
   }
   form.classList.add("d-none");
   kont.classList.remove("d-none");
+  section_ball.append(ball);
+  section_ball.append(ball2);
   //появляется первый уровень
   show_pictures1();
 });
@@ -45,18 +59,24 @@ binput_form.addEventListener("click", function () {
 brestart.addEventListener("click", function () {
   perehod.classList.add("d-none");
   numb1.classList.remove("d-none");
+  user_cscore -=user_cscore1;
+  user_cscore1 = 0;
   show_pictures1();
 });
 // возврат на 2 уровень
 brestart2.addEventListener("click", function () {
   perehod2.classList.add("d-none");
   numb2.classList.remove("d-none");
+  user_cscore -=user_cscore2;
+  user_cscore2 = 0;
   show_pictures2();
 });
 // возврат на 3 уровень
 brestart3.addEventListener("click", function () {
   perehod3.classList.add("d-none");
   numb3.classList.remove("d-none");
+  user_cscore -=user_cscore3;
+  user_cscore3 = 0;
   show_pictures3();
 });
 
@@ -76,6 +96,10 @@ bgo2.addEventListener("click", function () {
 
 //работа 1 уровня
 function show_pictures1() {
+  ball2.innerHTML = `<p>баллы общ: ${user_cscore}</p>`;
+
+  ball.innerHTML = `<p>баллы за уровень: ${user_cscore1}</p>`;
+
   let rand = Math.floor(Math.random() * questions1.length);
 
   numb1.innerHTML = `<h1>Уровень 1</h1>
@@ -85,7 +109,7 @@ function show_pictures1() {
   let count = addImg(questions1[rand].key, numb1);
 
   // обработка клика по картинкам
-  let dict_elem = document.querySelectorAll(".elem");
+  let dict_elem = document.querySelectorAll(".elem-image");
   let newcount = 0;
 
   const statistic = document.createElement("div");
@@ -99,23 +123,33 @@ function show_pictures1() {
         if (!this.classList.contains("true")) {
           newcount++;
           this.classList.add("true");
+          user_cscore1 +=10;
         }
       } else {
         this.classList.add("false");
+        user_cscore1 -=5;
       }
 
-      statistic.innerHTML = `<p>${newcount} / ${count}</p>`;
+      statistic.innerHTML = ` <p>${newcount} / ${count}</p>`;
+      ball.innerHTML = `<p>баллы за уровень: ${user_cscore1}</p>`;
 
       if (newcount === count) {
         numb1.classList.add("d-none");
         numb1.innerHTML = "";
         perehod.classList.remove("d-none");
+        user_cscore =user_cscore + user_cscore1;
+        ball2.innerHTML = `<p>баллы общ: ${user_cscore}</p>`;
       }
     });
   }
+  
 }
 
 function show_pictures2() {
+  ball2.innerHTML = `<p>баллы общ: ${user_cscore}</p>`;
+
+  ball.innerHTML = `<p>баллы за уровень: ${user_cscore2}</p>`;
+
   let rand = Math.floor(Math.random() * questions1.length);
   num2_task.innerHTML = `<h1>Уровень 2</h1>
                      <h2>${uname.value}, ${questions1[rand].que}</h2>`;
@@ -123,8 +157,8 @@ function show_pictures2() {
   let count = addImg(questions1[rand].key, num2_task);
 
   //обработка перетаскивания
-  let dict_elem = document.querySelectorAll(".elem");
-  let flel = document.querySelector(".flex-wrap");
+  let dict_elem = document.querySelectorAll(".elem-image");
+  let flel = document.querySelector(".flex-image");
 
   // Перебираем все элементы списка и присваиваем нужное значение
   for (const task of dict_elem) {
@@ -149,54 +183,80 @@ function show_pictures2() {
     event.target.append(document.querySelector(".selected"));
   }
 
-  let bresultat = document.querySelector(".resultat");
+  let bresultat = document.querySelector(".bresultat");
   bresultat.addEventListener("click", function () {
-    let itemRes = document.querySelectorAll(".res > .elem");
+    let itemRes = document.querySelectorAll(".res > .elem-image");
     let newcounter = 0;
+    user_cscore2 = 0;
     for (let i = 0; i < itemRes.length; i++) {
       if (itemRes[i].getAttribute("data-text") === questions1[rand].key) {
+        itemRes[i].classList.add("true");
+        user_cscore2 +=10;
         newcounter++;
       }
+      else {
+        user_cscore2 -=5;
+      }
+      ball.innerHTML = `<p>баллы за уровень: ${user_cscore2}</p>`;
       if (newcounter === count) {
         numb2.classList.add("d-none");
         num2_task.innerHTML = "";
         num2_res.innerHTML = "";
         perehod2.classList.remove("d-none");
+        user_cscore += user_cscore2;
+        ball.innerHTML = `<p>баллы за уровень: ${user_cscore2}</p>`;
+        ball2.innerHTML = `<p>баллы общ: ${user_cscore}</p>`;
       }
     }
   });
 }
 
 function show_pictures3() {
+  ball2.innerHTML = `<p>баллы общ: ${user_cscore}</p>`;
+
+  ball.innerHTML = `<p>баллы за уровень: ${user_cscore3}</p>`;
+
   let rand = Math.floor(Math.random() * questions1.length);
-  num3_task.innerHTML = `<h1>Уровень 2</h1>
+  num3_task.innerHTML = `<h1>Уровень 3</h1>
                      <h2>${uname.value}, ${questions1[rand].que}</h2>`;
 
   let count = addImg(questions1[rand].key, num3_task);
 
-  //обработка перетаскивания
-  let dict_elem = document.querySelectorAll(".elem");
+  //обработка дв клика
+  let dict_elem = document.querySelectorAll(".elem-image");
 
   for (let i = 0; dict_elem.length > i; i++) {
     dict_elem[i].addEventListener("dblclick", function () {
       const append_el = this.cloneNode(true);
       num3_res.append(append_el);
+      dict_elem[i].remove();
     });
   }
 
-  let bresultat = document.querySelector(".resultat3");
+  let bresultat = document.querySelector(".bresultat3");
   bresultat.addEventListener("click", function () {
-    let itemRes = document.querySelectorAll(".res3 > .elem");
+    let itemRes = document.querySelectorAll(".res3 > .elem-image");
     let newcounter = 0;
+    user_cscore3 = 0;
     for (let i = 0; i < itemRes.length; i++) {
       if (itemRes[i].getAttribute("data-text") === questions1[rand].key) {
+        itemRes[i].classList.add("true");
+        console.log(0);
+        user_cscore3 +=10;
         newcounter++;
       }
+      else {
+        user_cscore3 -=5;
+      }
+      ball.innerHTML = `<p>баллы за уровень: ${user_cscore3}</p>`;
       if (newcounter === count) {
         numb3.classList.add("d-none");
         num3_task.innerHTML = "";
         num3_res.innerHTML = "";
         perehod3.classList.remove("d-none");
+        user_cscore += user_cscore3;
+        ball.innerHTML = `<p>баллы за уровень: ${user_cscore3}</p>`;
+        ball2.innerHTML = `<p>баллы общ: ${user_cscore}</p>`;
       }
     }
   });
@@ -204,7 +264,7 @@ function show_pictures3() {
 
 function addImg(val, tegd) {
   const tdiv = document.createElement("div");
-  tdiv.classList.add("flex-wrap");
+  tdiv.classList.add("flex-image");
   let count = 0;
   let elemmass = [];
   for (let i = 1; i <= 9; i++) {
@@ -233,7 +293,7 @@ function addImg(val, tegd) {
       if (mastype === val) count++;
 
       const tdiv2 = document.createElement("div");
-      tdiv2.classList.add("elem");
+      tdiv2.classList.add("elem-image");
       tdiv2.setAttribute("data-text", mastype);
       tdiv2.style.backgroundImage = `url('${mass[rand2].img}')`;
       tdiv.append(tdiv2);
