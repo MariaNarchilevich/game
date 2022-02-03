636d5c7523519ff922ca50545465b75c8651d7d5
MariaNarchilevich <87192847+MariaNarchilevich@users.noreply.github.com>
2022-02-03 23:29:49 +0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 let button_start = document.querySelector(".bstart"); //кнопка старта
 let binput_form = document.querySelector(".binput"); //кнопка отправки формы
-let brestart = document.querySelector(".brestart"); //кнопка занова уровень 
-let bgo = document.querySelector(".bgo"); //кнопка переход на уровень 
-let brest_ur = document.querySelector(".brestart-ur");
+let breturn = document.querySelector(".breturn"); //кнопка возврат уровень
+let bgo = document.querySelector(".bgo"); //кнопка переход на уровень
+let brest_ur = document.querySelector(".brestart-ur"); //кнопка рестарта уровня 
+let bresultat2 = document.querySelector(".bresultat2");
+let bresultat3 = document.querySelector(".bresultat3");
 
 let form = document.querySelector(".forma"); //форма
 let kont = document.querySelector(".conteiner"); //фон заданий
@@ -12,6 +14,7 @@ let numb3 = document.querySelector(".number3"); //2задание
 
 let uname = document.querySelector(".user-name"); //имя пользователя
 
+let start_str = document.querySelector(".start-str");
 let perehod = document.querySelector(".perehod"); //переход
 let num2_task = document.querySelector(".flex-column"); //2задание
 let num2_res = document.querySelector(".res"); //2 задание
@@ -30,11 +33,15 @@ ball2.classList.add("score2");
 const ball = document.createElement("div");
 ball.classList.add("score");
 
+// вспомогательные переменные
 let k = 0;
+let rand = 0;
+let count = 0;
+
 
 // старт
 button_start.addEventListener("click", function () {
-  button_start.classList.add("d-none");
+  start_str.classList.add("d-none");
   form.classList.remove("d-none");
 });
 
@@ -54,31 +61,46 @@ binput_form.addEventListener("click", function () {
   show_pictures1();
 });
 
-// занова уровень
-brest_ur.addEventListener('click', function(){
-  if (document.querySelector(".number1.d-none")===null) { show_pictures1();}
-  if (document.querySelector(".number2.d-none")===null) {
+// рестарт уровень
+brest_ur.addEventListener("click", function () {
+  if (document.querySelector(".number1.d-none") === null) {
+    show_pictures1();
+  }
+  if (document.querySelector(".number2.d-none") === null) {
     num2_res.innerHTML = "";
     num2_task.innerHTML = "";
-    show_pictures2();}
-  if (document.querySelector(".number3.d-none")===null) { 
+    bresultat2.removeEventListener("click", f2);
+    show_pictures2();
+  }
+  if (document.querySelector(".number3.d-none") === null) {
     num3_res.innerHTML = "";
     num3_task.innerHTML = "";
-    show_pictures3();}
-})
+    bresultat3.removeEventListener("click", f3);
+    show_pictures3();
+  }
+});
 
-
-// типо возврат на предыдущий уровень 
-brestart.addEventListener("click", function () {
-  if (k===1) { restart(numb1,user_cscore_ur, show_pictures1); }
-  if (k===2) { restart(numb2,user_cscore_ur, show_pictures2); }
-  if (k===3) { restart(numb3,user_cscore_ur, show_pictures3); }
+// типо возврат на предыдущий уровень
+breturn.addEventListener("click", function () {
+  if (k === 1) {
+    returnUr(numb1, user_cscore_ur, show_pictures1);
+  }
+  if (k === 2) {
+    returnUr(numb2, user_cscore_ur, show_pictures2, f2, bresultat2);
+  }
+  if (k === 3) {
+    returnUr(numb3, user_cscore_ur, show_pictures3, f3, bresultat3);
+  }
 });
 
 //переход на след уровень
 bgo.addEventListener("click", function () {
-  if (k===1) { goo(numb2, show_pictures2)}
-  if (k===2) { goo(numb3, show_pictures3)}
+  if (k === 1) {
+    goo(numb2, show_pictures2);
+  }
+  if (k === 2) {
+    goo(numb3, show_pictures3);
+  }
 });
 
 //работа 1 уровня
@@ -89,13 +111,13 @@ function show_pictures1() {
 
   ball.innerHTML = `<p>штрафные баллы за уровень: ${score_shtraf}</p>`;
 
-  let rand = Math.floor(Math.random() * questions1.length);
+  rand = Math.floor(Math.random() * questions1.length);
 
   numb1.innerHTML = `<h1>Уровень 1</h1>
                      <h2>${uname.value}, ${questions1[rand].que}</h2>`;
 
   //появляются картинки
-  let count = addImg(questions1[rand].key, numb1);
+  count = addImg(questions1[rand].key, numb1);
 
   // обработка клика по картинкам
   let dict_elem = document.querySelectorAll(".elem-image");
@@ -115,7 +137,7 @@ function show_pictures1() {
         }
       } else {
         this.classList.add("false");
-        score_shtraf +=5;
+        score_shtraf += 5;
       }
 
       statistic.innerHTML = ` <p>${newcount} / ${count}</p>`;
@@ -126,30 +148,29 @@ function show_pictures1() {
         numb1.innerHTML = "";
         brest_ur.classList.add("d-none");
         perehod.classList.remove("d-none");
-        k=1;
+        k = 1;
         user_cscore_ur -= score_shtraf;
-        user_cscore +=user_cscore_ur;
+        user_cscore += user_cscore_ur;
         ball.innerHTML = `<p>баллы за уровень: ${user_cscore_ur}</p>`;
         ball2.innerHTML = `<p>баллы общ: ${user_cscore}</p>`;
       }
     });
   }
-  
 }
-let bresultat = document.querySelector(".bresultat");
+
 function show_pictures2() {
   user_cscore_ur = 40;
   score_shtraf = 0;
-  
+
   ball2.innerHTML = `<p>баллы общ: ${user_cscore}</p>`;
 
   ball.innerHTML = `<p>штрафные баллы за уровень: ${score_shtraf}</p>`;
 
-  let rand = Math.floor(Math.random() * questions1.length);
+  rand = Math.floor(Math.random() * questions1.length);
   num2_task.innerHTML = `<h1>Уровень 2</h1>
                      <h2>${uname.value}, ${questions1[rand].que}</h2> <p>(перетащи их в красную зону)</p>`;
 
-  let count = addImg(questions1[rand].key, num2_task);
+  count = addImg(questions1[rand].key, num2_task);
 
   //обработка перетаскивания
   let dict_elem = document.querySelectorAll(".elem-image");
@@ -167,14 +188,12 @@ function show_pictures2() {
     evt.target.classList.add(`selected`);
   });
 
-
   flel.addEventListener(`dragend`, (evt) => {
     evt.target.classList.remove(`selected`);
   });
   num2_res.addEventListener(`dragend`, (evt) => {
     evt.target.classList.remove(`selected`);
   });
-  
 
   num2_res.ondragover = allowDrop;
   num2_task.ondragover = allowDrop;
@@ -188,50 +207,52 @@ function show_pictures2() {
     event.target.append(document.querySelector(".selected"));
   }
 
-  
-  bresultat.addEventListener("click", function () {
-    let itemRes = document.querySelectorAll(".res > .elem-image");
-    let newcounter = 0;
-let d=0;
-    for (let i = 0; i < itemRes.length; i++) {
-      console.log(d++);
-      if (itemRes[i].getAttribute("data-text") === questions1[rand].key) {
-        itemRes[i].classList.add("true");
-        newcounter++;
-      }
-      if (itemRes[i].getAttribute("data-text") !== questions1[rand].key) {
-        score_shtraf +=5;
-      }
+  bresultat2.addEventListener("click", f2);
+}
+
+function f2() {
+  let itemRes = document.querySelectorAll(".res > .elem-image");
+  let newcounter = 0;
+  // score_shtraf = 0;
+  let d = 0;
+  for (let i = 0; i < itemRes.length; i++) {
+    console.log(d++);
+    if (itemRes[i].getAttribute("data-text") === questions1[rand].key) {
+      itemRes[i].classList.add("true");
+      newcounter++;
     }
-    ball.innerHTML = `<p>штрафные баллы за уровень: ${score_shtraf}</p>`;
-      if (newcounter === count) {
-        num2_task.innerHTML = "";
-        num2_res.innerHTML = "";
-        numb2.classList.add("d-none");
-        brest_ur.classList.add("d-none");
-        perehod.classList.remove("d-none");
-        k=2;
-        user_cscore_ur -= score_shtraf;
-        user_cscore += user_cscore_ur;
-        ball.innerHTML = `<p>баллы за уровень: ${user_cscore_ur}</p>`;
-        ball2.innerHTML = `<p>баллы общ: ${user_cscore}</p>`;
-      }
-  });
+    if (itemRes[i].getAttribute("data-text") !== questions1[rand].key) {
+      score_shtraf += 5;
+    }
+  }
+  ball.innerHTML = `<p>штрафные баллы за уровень: ${score_shtraf}</p>`;
+  if (newcounter === count) {
+    num2_task.innerHTML = "";
+    num2_res.innerHTML = "";
+    numb2.classList.add("d-none");
+    brest_ur.classList.add("d-none");
+    perehod.classList.remove("d-none");
+    k = 2;
+    user_cscore_ur -= score_shtraf;
+    user_cscore += user_cscore_ur;
+    ball.innerHTML = `<p>баллы за уровень: ${user_cscore_ur}</p>`;
+    ball2.innerHTML = `<p>баллы общ: ${user_cscore}</p>`;
+  }
 }
 
 function show_pictures3() {
   user_cscore_ur = 40;
   score_shtraf = 0;
-  
+
   ball2.innerHTML = `<p>баллы общ: ${user_cscore}</p>`;
 
   ball.innerHTML = `<p>штрафные баллы за уровень: ${score_shtraf}</p>`;
 
-  let rand = Math.floor(Math.random() * questions1.length);
+  rand = Math.floor(Math.random() * questions1.length);
   num3_task.innerHTML = `<h1>Уровень 3</h1>
                      <h2>${uname.value}, ${questions1[rand].que}</h2> <p>два раза нажми на картинку</p>`;
 
-  let count = addImg(questions1[rand].key, num3_task);
+  count = addImg(questions1[rand].key, num3_task);
 
   //обработка дв клика
   let dict_elem = document.querySelectorAll(".elem-image");
@@ -244,36 +265,34 @@ function show_pictures3() {
     });
   }
 
-  let bresultat = document.querySelector(".bresultat3");
-  bresultat.addEventListener("click", function () {
-    let itemRes = document.querySelectorAll(".res3 > .elem-image");
-    let newcounter = 0;
-    for (let i = 0; i < itemRes.length; i++) {
-      if (itemRes[i].getAttribute("data-text") === questions1[rand].key) {
-        itemRes[i].classList.add("true");
-        newcounter++;
-      }
-      else {
-        score_shtraf +=5;
-      }
-    }
-    ball.innerHTML = `<p>штрафные баллы за уровень: ${score_shtraf}</p>`;
-      if (newcounter === count) {
-        num3_task.innerHTML = "";
-        num3_res.innerHTML = "";
-        numb3.classList.add("d-none");
-        brest_ur.classList.add("d-none");
-        k=3;
-        perehod.classList.remove("d-none");
-        bgo.classList.add("d-none");
-        user_cscore_ur -= score_shtraf;
-        user_cscore += user_cscore_ur;
-        ball.innerHTML = `<p>баллы за уровень: ${user_cscore_ur}</p>`;
-        ball2.innerHTML = `<p>баллы общ: ${user_cscore}</p>`;
-      }
-  });
+  bresultat3.addEventListener("click", f3);
 }
-
+function f3() {
+  let itemRes = document.querySelectorAll(".res3 > .elem-image");
+  let newcounter = 0;
+  for (let i = 0; i < itemRes.length; i++) {
+    if (itemRes[i].getAttribute("data-text") === questions1[rand].key) {
+      itemRes[i].classList.add("true");
+      newcounter++;
+    } else {
+      score_shtraf += 5;
+    }
+  }
+  ball.innerHTML = `<p>штрафные баллы за уровень: ${score_shtraf}</p>`;
+  if (newcounter === count) {
+    num3_task.innerHTML = "";
+    num3_res.innerHTML = "";
+    numb3.classList.add("d-none");
+    brest_ur.classList.add("d-none");
+    k = 3;
+    perehod.classList.remove("d-none");
+    bgo.classList.add("d-none");
+    user_cscore_ur -= score_shtraf;
+    user_cscore += user_cscore_ur;
+    ball.innerHTML = `<p>баллы за уровень: ${user_cscore_ur}</p>`;
+    ball2.innerHTML = `<p>баллы общ: ${user_cscore}</p>`;
+  }
+}
 function addImg(val, tegd) {
   const tdiv = document.createElement("div");
   tdiv.classList.add("flex-image");
@@ -316,16 +335,17 @@ function addImg(val, tegd) {
   return count;
 }
 
-function restart (num, us, func) {
+function returnUr(num, us, func, cn, re) {
   perehod.classList.add("d-none");
   num.classList.remove("d-none");
-  user_cscore -=us;
+  user_cscore -= us;
   brest_ur.classList.remove("d-none");
+  re.removeEventListener("click", cn);
   func();
   return 0;
 }
 
-function goo(num,func) {
+function goo(num, func) {
   perehod.classList.add("d-none");
   num.classList.remove("d-none");
   brest_ur.classList.remove("d-none");
@@ -335,102 +355,102 @@ function goo(num,func) {
 let mass = [
   {
     name: "юла",
-    img: "img/юла.jpg",
+    img: "img/whirligig.jpg",
     type: "игрушки",
   },
   {
     name: "бананы",
-    img: "img/бананы.jpg",
+    img: "img/banana.jpg",
     type: "еда",
   },
   {
     name: "ботинки",
-    img: "img/ботинки.jpg",
+    img: "img/boots.jpg",
     type: "одежда",
   },
   {
     name: "варежки",
-    img: "img/варежки.jpg",
+    img: "img/mittens.jpg",
     type: "одежда",
   },
   {
     name: "комбенизон",
-    img: "img/комбез.jpg",
+    img: "img/overalls.jpg",
     type: "одежда",
   },
   {
     name: "конфета",
-    img: "img/конфета.jpg",
+    img: "img/candy.jpg",
     type: "еда",
   },
   {
     name: "корова",
-    img: "img/корова.jpg",
+    img: "img/cow.jpg",
     type: "животные",
   },
   {
     name: "кот",
-    img: "img/кот.jpg",
+    img: "img/cat.jpg",
     type: "животные",
   },
   {
     name: "кукла",
-    img: "img/кукла.jpg",
+    img: "img/doll.jpg",
     type: "игрушки",
   },
   {
     name: "машина",
-    img: "img/машина.jpg",
+    img: "img/car.jpg",
     type: "транспорт",
   },
   {
     name: "мышка",
-    img: "img/мышка.jpg",
+    img: "img/mouse.jpg",
     type: "животные",
   },
   {
     name: "мяч",
-    img: "img/мяч.jpg",
+    img: "img/ball.jpg",
     type: "игрушки",
   },
   {
     name: "паровоз",
-    img: "img/паровоз.jpg",
+    img: "img/locomotive.jpg",
     type: "транспорт",
   },
   {
     name: "пирамидка",
-    img: "img/пирамидка.jpg",
+    img: "img/pyramid.jpg",
     type: "игрушки",
   },
   {
     name: "самолет",
-    img: "img/самолет.jpg",
+    img: "img/airplane.jpg",
     type: "транспорт",
   },
   {
     name: "слон",
-    img: "img/слон.jpg",
+    img: "img/elephant.jpg",
     type: "животные",
   },
   {
     name: "торт",
-    img: "img/торт.jpg",
+    img: "img/cake.jpg",
     type: "еда",
   },
   {
     name: "трактор",
-    img: "img/трактор.jpg",
+    img: "img/tractor.jpg",
     type: "транспорт",
   },
   {
     name: "хлеб",
-    img: "img/хлеб.jpg",
+    img: "img/bread.jpg",
     type: "еда",
   },
   {
     name: "шляпа",
-    img: "img/шляпа.jpg",
+    img: "img/hat.jpg",
     type: "одежда",
   },
 ];
